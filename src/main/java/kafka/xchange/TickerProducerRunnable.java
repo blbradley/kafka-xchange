6e8c96d74e9dc0bf5d28ca260979263d73e666d8
Brandon Bradley <bradleytastic@gmail.com>
2015-08-01 12:55:05 -0500

    use Jackson to convert exchange data types to json
    Jackson is already a transitive dependency.

--- src/main/java/kafka/xchange/TickerProducerRunnable.java
+++ src/main/java/kafka/xchange/TickerProducerRunnable.java
@@ -1,25 +1,25 @@
 package kafka.xchange;
 
-import org.json.JSONObject;
-
 import java.io.IOException;
 import java.lang.RuntimeException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import kafka.javaapi.producer.Producer;
 
 import com.xeiam.xchange.Exchange;
 import com.xeiam.xchange.dto.marketdata.Ticker;
 import com.xeiam.xchange.currency.CurrencyPair;
-import com.xeiam.xchange.utils.DateUtils;
 
 interface TickerTopic {
     static String topicName = "ticks";
 }
 
-class TickerProducerRunnable extends PollingExchangeProducerRunnable implements TickerTopic {
+class TickerProducerRunnable extends PollingExchangeProducerRunnable implements TickerTopic, ObjectMapperInjector {
+    private static final Logger logger = LoggerFactory.getLogger(TickerProducerRunnable.class);
 
     TickerProducerRunnable(Producer<String, String> producer,
             Exchange exchange) {
@@ -33,23 +33,12 @@ class TickerProducerRunnable extends PollingExchangeProducerRunnable implements 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        String msg = tickerToJSON(ticker).toString();
-        send(TickerProducerRunnable.topicName, msg);
-    }
-
-    public static JSONObject tickerToJSON(Ticker ticker) {
-        JSONObject json = new JSONObject();
-
-        json.put("pair", ticker.getCurrencyPair());
-        json.put("last", ticker.getLast());
-        json.put("bid", ticker.getBid());
-        json.put("ask", ticker.getAsk());
-        json.put("high", ticker.getHigh());
-        json.put("low", ticker.getLow());
-        json.put("avg", ticker.getVwap());
-        json.put("volume", ticker.getVolume());
-        json.put("timestamp", DateUtils.toMillisNullSafe(ticker.getTimestamp()));
-
-        return json;
+        String msg;
+        try {
+            msg = mapper.writeValueAsString(ticker);
+            send(TickerProducerRunnable.topicName, msg);
+        } catch (JsonProcessingException e) {
+            logger.error(e.getClass().getName() + ":" + e.getMessage());
+        }
     }
 }
